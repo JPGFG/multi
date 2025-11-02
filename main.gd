7dfdc4d984c2eda36ce29d11b68e1e62472c865f
JP <jp.greasefiregames@gmail.com>
2025-11-02 16:14:34 -0500

    authoritative headless server complete pt 1

--- main.gd
+++ main.gd
@@ -1,19 +1,48 @@
+# main.gd
 extends Node2D
 
 var player_scene = preload("res://scenes/player.tscn")
+
+var is_server: bool = false
+var server: Server
+var client: Client
 
 func _ready() -> void:
 	var args = OS.get_cmdline_user_args()
 	if "--server" in args:
 		print("Starting in SERVER mode")
+		is_server = true
 		var net_server_scene = load("res://scenes/server.tscn")
-		var net_server = net_server_scene.instantiate()
-		add_child(net_server)
+		server = net_server_scene.instantiate()
+		add_child(server)
 	else:
 		print("Staring in CLIENT mode")
-		var net_client = Client.new() as Node
-		add_child(net_client)
+		is_server = false
+		client = Client.new() as Node
+		add_child(client)
 		
 		var player = player_scene.instantiate()
 		player.position = Vector2.ZERO
 		add_child(player)
+		
+		client.register_local_player(player)
+
+
+# Shared RPCs
+
+# CLIENT -> SERVER
+@rpc("any_peer", "unreliable")
+func c2s_input(dir: Vector2) -> void:
+	if not is_server:
+		return
+	# we are on the server here
+	var from_id = multiplayer.get_remote_sender_id()
+	server.handle_client_input(from_id, dir)
+
+#SERVER -> CLIENTS
+@rpc("unreliable")
+func s2c_state(state: Dictionary) -> void:
+	if is_server:
+		return
+	# client only code
+	client.apply_state(state)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
+# player.gd
 class_name Player
 extends CharacterBody2D
 
-const SPEED = 450
 
 func _physics_process(delta):
 	var input_dir: Vector2 = Vector2.ZERO
@@ -14,5 +14,14 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		input_dir.y -= 1
 	
-	if multiplayer.get_unique_id() != 1:
-		rpc_id(1, "c2s_input", input_dir)
+	if multiplayer.is_server():
+		return
+	
+	var mp = multiplayer.get_multiplayer_peer()
+	if mp == null:
+		return
+	if mp.get_connection_status() != MultiplayerPeer.CONNECTION_CONNECTED:
+		return
+	
+	var main = get_tree().root.get_node("Main")
+	main.rpc_id(1, "c2s_input", input_dir)
